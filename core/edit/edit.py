d52dd68bd6ef26384db26049edea53945ad68f44
Mike Liao <mikeliao97@gmail.com>
2023-04-22 14:24:43 -0700

    add some more things

--- core/edit/edit.py
+++ core/edit/edit.py
@@ -67,6 +67,48 @@ class EditActions:
         actions.edit.word_left()
         actions.edit.extend_word_right()
 
+    def down_n(n: int):
+        """Goes down n lines"""
+        for _ in range(n):
+            actions.edit.down()
+            actions.sleep("10ms")
+
+    def up_n(n: int):
+        """Goes up n lines"""
+        for _ in range(n):
+            actions.edit.up()
+            actions.sleep("10ms")
+
+    def left_n(n: int):
+        """Goes left n lines"""
+        for _ in range(n):
+            actions.edit.left()
+
+    def delete_left_n(n: int):
+        """Goes left n lines"""
+        actions.key(f"backspace:{n}")
+
+    def delete_right_n(n: int):
+        """Goes left n lines"""
+        actions.key(f"delete:{n}")
+
+    def right_n(n: int):
+        """Goes right n lines"""
+        for _ in range(n):
+            actions.edit.right()
+
+    def delete_word_right_n(n: int):
+        """Delete right n words"""
+        for _ in range(n):
+            actions.edit.extend_word_right()
+        actions.edit.delete()
+
+    def delete_word_left_n(n: int):
+        """Delete left n words"""
+        for _ in range(n):
+            actions.edit.extend_word_left()
+        actions.edit.delete()
+
 
 @mod.action_class
 class Actions:
